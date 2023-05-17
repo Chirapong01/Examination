9e943a64250631778937e6da10b334d26a112ef2
Chirapong01 <74916492+Chirapong01@users.noreply.github.com>
2023-05-17 16:42:27 +0700

    add share and model butnow

--- src/product/scriptProduct.js
+++ src/product/scriptProduct.js
@@ -4,27 +4,29 @@ let search = document.getElementsByClassName("search")
 let navlist = document.getElementsByClassName("nav-list")
 let logo = document.getElementsByClassName("logo")
 let navicon = document.getElementsByClassName("nav-icon")
+let piece = document.getElementById("count").value
 let lasScrollY = window.scrollY
+let dollarUSLocale = Intl.NumberFormat('en-US');
 
 
 window.addEventListener("scroll", () => {
     if (lasScrollY > 50) {
-        navlist[0].style.marginTop="25px";
-        search[0].style.display ="none"
-        topnav[0].style.height="70px"
-        logo[0].style.display ="none"
-        navicon[0].style.marginTop ="0px"
-        navicon[1].style.marginTop ="0px"
-        navicon[2].style.marginTop ="0px"
+        navlist[0].style.marginTop = "25px";
+        search[0].style.display = "none"
+        topnav[0].style.height = "70px"
+        logo[0].style.display = "none"
+        navicon[0].style.marginTop = "0px"
+        navicon[1].style.marginTop = "0px"
+        navicon[2].style.marginTop = "0px"
         nav.classList.add("nav-fix")
     } else {
-        navlist[0].style.marginTop="1rem";
-        search[0].style.display ="flex"
-        topnav[0].style.height="auto"
-        logo[0].style.display ="block"
-        navicon[0].style.marginTop ="0px"
-        navicon[1].style.marginTop ="0px"
-        navicon[2].style.marginTop ="0px"
+        navlist[0].style.marginTop = "1rem";
+        search[0].style.display = "flex"
+        topnav[0].style.height = "auto"
+        logo[0].style.display = "block"
+        navicon[0].style.marginTop = "0px"
+        navicon[1].style.marginTop = "0px"
+        navicon[2].style.marginTop = "0px"
         nav.classList.remove("nav-fix")
     }
     lasScrollY = window.scrollY
@@ -76,17 +78,27 @@ function arrowChange(index) {
 }
 
 function quantity(index) {
-    let i = document.getElementById("count").value
-    if (index == 'add') {
-        if (i < 1000) {
-            i++
-        }
+    let total
+    if (piece < 0) {
+        piece = 0
     } else {
-        if (i > 0) {
-            i--
+        if (index == 'add') {
+            if (piece < 1000) {
+                piece++
+            }
+        } else if (index == 'remove') {
+            if (piece > 0) {
+                piece--
+            }
+        }
+        else if (index == 'change') {
+                piece = document.getElementById("count").value
         }
     }
-    document.getElementById("count").value = i
+    document.getElementById("count").value = piece
+    total = piece * 11400
+    document.getElementById("piece-model").innerHTML = piece
+    document.getElementById("total-model").innerHTML = dollarUSLocale.format(total)
 }
 
 function selectDescription(index) {
@@ -119,7 +131,7 @@ function opensidebar(index) {
         console.log(data);
         data[0].classList.add("display-show")
         document.body.classList.add("stop-scrolling");
-    }else if(index =='close'){
+    } else if (index == 'close') {
         data[0].classList.remove("display-show")
         document.body.classList.remove("stop-scrolling");
     }
@@ -128,95 +140,95 @@ function opensidebar(index) {
 
 const sliderRecently = document.querySelector(".contrain-recently")
 const sliderRecommended = document.querySelector(".contrain-recommend")
-let isDrag=false
+let isDrag = false
 let prevPageX
 let prevScroll
 
-function drageStop(){
+function drageStop() {
     isDrag = false;
 }
 
-function drageRecentlyStart(e){
+function drageRecentlyStart(e) {
     isDrag = true;
     prevPageX = e.pageX
     prevScroll = sliderRecently.scrollLeft
 }
 
-function recentlyDragging (e){
+function recentlyDragging(e) {
     let position = e.pageX - prevPageX
-    if(isDrag == true){
+    if (isDrag == true) {
         e.preventDefault()
         sliderRecently.scrollLeft = prevScroll - position
-    }else{
+    } else {
         return
     }
-   
+
 }
 
-function drageRecommendedStart(e){
+function drageRecommendedStart(e) {
     isDrag = true;
     prevPageX = e.pageX
     prevScroll = sliderRecommended.scrollLeft
 }
 
-function recommendedDragging (e){
+function recommendedDragging(e) {
     let position = e.pageX - prevPageX
-    if(isDrag == true){
+    if (isDrag == true) {
         e.preventDefault()
         sliderRecommended.scrollLeft = prevScroll - position
-    }else{
+    } else {
         return
     }
-   
+
 }
-function phoneDrageRecentlyStart(e){
+function phoneDrageRecentlyStart(e) {
     isDrag = true;
     prevPageX = e.touches[0].pageX
     prevScroll = sliderRecently.scrollLeft
- 
+
 }
 
-function phoneRecentlyDragging (e){
+function phoneRecentlyDragging(e) {
     let position = e.touches[0].pageX - prevPageX
-    if(isDrag == true){
+    if (isDrag == true) {
         e.preventDefault()
         sliderRecently.scrollLeft = prevScroll - position
-    }else{
+    } else {
         return
     }
-   
+
 }
 
-function phoneDrageRecommendedStart(e){
+function phoneDrageRecommendedStart(e) {
     isDrag = true;
     prevPageX = e.touches[0].pageX
     prevScroll = sliderRecommended.scrollLeft
- 
+
 }
 
-function phoneRecommendedDragging (e){
+function phoneRecommendedDragging(e) {
     let position = e.touches[0].pageX - prevPageX
-    if(isDrag == true){
+    if (isDrag == true) {
         e.preventDefault()
         sliderRecommended.scrollLeft = prevScroll - position
-    }else{
+    } else {
         return
     }
-   
+
 }
 
 
-sliderRecently.addEventListener("mousedown",drageRecentlyStart)
-sliderRecently.addEventListener("mouseup",drageStop)
-sliderRecently.addEventListener("mouseleave",drageStop)
-sliderRecently.addEventListener("mousemove",recentlyDragging)
-sliderRecently.addEventListener("touchstart",phoneDrageRecentlyStart)
-sliderRecently.addEventListener("touchend",drageStop)
-sliderRecently.addEventListener("touchmove",phoneRecentlyDragging)
-sliderRecommended.addEventListener("mousedown",drageRecommendedStart)
-sliderRecommended.addEventListener("mouseup",drageStop)
-sliderRecommended.addEventListener("mouseleave",drageStop)
-sliderRecommended.addEventListener("mousemove",recommendedDragging)
-sliderRecommended.addEventListener("touchstart",phoneDrageRecommendedStart)
-sliderRecommended.addEventListener("touchend",drageStop)
-sliderRecommended.addEventListener("touchmove",phoneRecommendedDragging)
+sliderRecently.addEventListener("mousedown", drageRecentlyStart)
+sliderRecently.addEventListener("mouseup", drageStop)
+sliderRecently.addEventListener("mouseleave", drageStop)
+sliderRecently.addEventListener("mousemove", recentlyDragging)
+sliderRecently.addEventListener("touchstart", phoneDrageRecentlyStart)
+sliderRecently.addEventListener("touchend", drageStop)
+sliderRecently.addEventListener("touchmove", phoneRecentlyDragging)
+sliderRecommended.addEventListener("mousedown", drageRecommendedStart)
+sliderRecommended.addEventListener("mouseup", drageStop)
+sliderRecommended.addEventListener("mouseleave", drageStop)
+sliderRecommended.addEventListener("mousemove", recommendedDragging)
+sliderRecommended.addEventListener("touchstart", phoneDrageRecommendedStart)
+sliderRecommended.addEventListener("touchend", drageStop)
+sliderRecommended.addEventListener("touchmove", phoneRecommendedDragging)
